1fafe1977cd83f5c5d6df0ca57234e948e858169
colosso97 <marcelovidal.ml@gmail.com>
2025-06-13 18:17:25 -0300

    edit:

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,3 @@
-
-
-import UncontrolledExample from "../../components/Carousel/UncontrolledExample"
-
-
 import UncontrolledExample from "../../components/Carousel/UncontrolledExample"
 import Header from "../../components/Header"
 import * as styles from './Home.module.css'
@@ -12,11 +7,8 @@ const Home = () => {
   return (
     <div className={styles.Header}>
     <Header />
-    <h1>oi</h1>
-    {/* <RecipeCarousel /> */}
     <UncontrolledExample/>
     <FooterMain/>
->>>>>>> main
     </div>
   )
 }
