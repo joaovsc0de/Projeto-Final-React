899d7fb20d97d5587a5e564655b19dbc531650ad
colosso97 <marcelovidal.ml@gmail.com>
2025-06-13 16:15:30 -0300

    Create: carousel Home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,14 @@
+
+
+import UncontrolledExample from "../../components/Carousel/UncontrolledExample"
 import Header from "../../components/Header"
-import RecipeCarousel from '../../components/Recipes/RecipeCarousel'
 import * as styles from './Home.module.css'
 
 const Home = () => {
   return (
     <div className={styles.Header}>
     <Header />
-    <h1>oi</h1>
-    {/* <RecipeCarousel /> */}
+    <UncontrolledExample/>
     </div>
   )
 }
